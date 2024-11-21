78b40c1c725f2ed6a518b72789129fb637909622
zainah7 <zainaabd7@gmail.com>
2024-11-21 21:05:45 +0100

    dashboard setup

---
+++ components/Navbar.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from "../img/logo.png";
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import ThemeToggler from "@/components/ThemeToggler";
+
+const Navbar = () => {
+  return (
+    <div className="bg-violet-950 dark:bg-slate-700 text-white py-2 px-5 flex justify-between">
+      import Link from 'next/link';
+      <Link href="/">
+        <h1 className="font-bold">zee</h1>
+      </Link>
+      <div className="flex items-center">
+        <ThemeToggler />
+        <DropdownMenu>
+          <DropdownMenuTrigger className="focus:outline-none">
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+              <AvatarFallback className="text-black">zee</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <Link href="/profile">Profile</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Link href="/auth">Logout</Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
